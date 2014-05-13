1d2be0173d29057af20154ec125fbda0b4a87f9f
aniversarioperu <aniversarioperu1@gmail.com>
2014-05-13 14:21:16 +0300

    start tema del dia

---
+++ editorial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Scrape newspaper editorial section of the day and guess the main subject"""
+
+
+def tema_del_dia():
+	print "aaaaa"
+
+def main():
+	tema_del_dia()
+
+
+if __name__ == "__main__":
+	main()

67dd372a5b78564c6d3601f062daeda47cf065fd
aniversarioperu <aniversarioperu1@gmail.com>
2014-05-13 15:02:40 +0300

    extract column

--- editorial.py
+++ editorial.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
-"""Scrape newspaper editorial section of the day and guess the main subject"""
+from BeautifulSoup import BeautifulSoup
+import requests
 
 
+# Scrape newspaper editorial section of the day and guess the main subject
 def tema_del_dia():
-	print "aaaaa"
+	today = datetime.today().strftime("%d-%m-%Y")
+	url = "http://www.larepublica.pe/politica/editorial-" + today
+	r = requests.get(url)
+
+	soup = BeautifulSoup(r.text)
+	print soup
+	for i in soup.find_all("div", "glr-post-social glr-mb-10"):
+		print i
+
 
 def main():
 	tema_del_dia()
